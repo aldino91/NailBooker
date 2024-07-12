295677381a2c48c23c8ccf57208b1431e9f548ae
Aldo Brunet <78109479+aldino91@users.noreply.github.com>
2024-07-12 20:59:44 +0200

    fix data fetch register

--- client/src/api/fetchRegister.ts
+++ client/src/api/fetchRegister.ts
@@ -14,11 +14,8 @@ export interface DataRegister {
 
 export const fetchRegister = async (data: DataRegister) => {
 	try {
-		const response = await axios.post(
-			`${import.meta.env.VITE_API_URL}/auth/register`,
-			// `https://nail-booker-rxhz.vercel.app/api/auth/register`,
-			data
-		);
+		const apiUrl = import.meta.env.VITE_API_URL;
+		const response = await axios.post(`${apiUrl}/auth/register`, data);
 
 		console.log('Response register  => ', response.data);
 		return response.data;
