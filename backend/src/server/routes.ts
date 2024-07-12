c37bac2c6a1efe87c84eeffcfa2b9378006d5fa3
Aldo Brunet <78109479+aldino91@users.noreply.github.com>
2024-07-12 18:28:35 +0200

    fix test route

--- backend/src/server/routes.ts
+++ backend/src/server/routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Response, Request, Router } from 'express';
 import { AuthRoutes } from '../model/users/presentations/routes';
 import { BooksRoutes } from '../model/books/presentations/routes';
 
@@ -9,6 +9,10 @@ export class AppRoutes {
 		router.use('/api', AuthRoutes.routes);
 
 		router.use('/api', BooksRoutes.routes);
+
+		router.use('/test', (req: Request, res: Response) => {
+			res.status(200).json({ message: 'test funziona' });
+		});
 
 		return router;
 	}
